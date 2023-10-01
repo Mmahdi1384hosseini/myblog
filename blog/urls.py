c08af8fabbf62b0216b1061031fbb12c39f1ea60
Mmahdi1384hosseini <hosseinim.m17@gmail.com>
2023-10-01 18:26:26 +0330

    My Django Girls app, first commit

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('create100/',views.create100),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.contrib.auth.models import User
 
-# Create your views here.
+def create100(requests):
+    me = User.objects.get(username='admin')
+    for i in range(100):
+        Post.objects.create(author=me, title='sport', text='football iran VS hongkong')
+
+    # p=Post()
+    # p.text='football iran VS hongkong'
+    # p.title='sport'
+    # p.author=User
+    # p.publish()
+    return HttpResponse("Done")
