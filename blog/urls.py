9ffe4c073b6ffcd3458aa443c730087de32cd09c
Mmahdi1384hosseini <hosseinim.m17@gmail.com>
2023-10-13 19:34:31 +0330

    f3

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
-
 urlpatterns = [
-    path('create100/',views.create100),
+
+    path('create100/', views.create100),
     path('2/',views.post_list2,name='post_list2'),
     path('',views.post_list,name='post_list'),
+     path('post/<int:pik>/', views.post_detail, name='post_detail'),
+     path('post/new/', views.post_new, name='post_new'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from django.http import HttpResponse
 from .models import Post
 from django.contrib.auth.models import User
+from .forms import PostForm 
+from django.utils import timezone
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_detail(request, pik):
+    post = get_object_or_404(Post, pk=pik)
+    return render(request, 'blog/post_detail.html', {'p': post})
 
 def post_list2(requests):
     ls=Post.objects.all()
     s="<ul>"
     for item in ls:
         s+="<li>"+str(item)+"</li>"
-    s+="</hl>"
+    s+="</ul>"
     return HttpResponse(s)
-
 
 def post_list(requests):
     ls=Post.objects.all()
-    content= {"posts":ls}
+    content = {"posts":ls}
     return render(requests,'blog/post_list.html',content)
+
 
 def create100(requests):
     me = User.objects.get(username='admin')
     for i in range(100):
-        p = Post.objects.create(author=me, title=f'sport{i}', text=f'football iran VS hongkong{i}')
+        p=Post.objects.create(author=me, title=f'fotball_omid{i}', text=f'fotball_omid dar barabar hongkong baxt {i}')
+
         p.publish()
     return HttpResponse("Done")
